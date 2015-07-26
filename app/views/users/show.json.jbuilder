6642ac0bd9c3632f98d48fc4ba274c6679dcd114
neurotroph <ich@neurotroph.de>
2015-07-26 11:07:49 +0200

    Initial commit

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :username, :name, :email, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Billable::Application.config.secret_key_base = '8f4b8557158ce6549e4209f8e067fbc13684efdc3cca0aec23551ddcd70c6c903fe87569b034c15f7c1b97c525c24aff7bdea5a519fc9a2bf8c48d6fb08f560d'
