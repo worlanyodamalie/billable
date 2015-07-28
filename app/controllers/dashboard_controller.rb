e2b9165da85975bd04a36a811e1291b320e4963c
neurotroph <ich@neurotroph.de>
2015-07-28 18:58:14 +0200

    Enhanced week view in Dashboard
    First iteration

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,14 @@ class DashboardController < ApplicationController
 
 	def index
 		@clients = Client.where(:user => User.find_by(:id => session[:user_id]))
+
+		day_names = %w[Monday Tuesday Wednesday Thursday Friday Saturday Sunday]
+		this_monday = Time.now.beginning_of_week
+		@week_days = Hash.new()
+		@week_data = Hash.new()
+		for i in 0..6
+			@week_days[day_names[i]] = this_monday + i.day
+		end
 	end
 
 end
